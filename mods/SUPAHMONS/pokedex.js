3296abfe987d41ff657fa1a82997073417c9a6be
Oiawesome <Oiawesome99@yahoo.com>
2013-07-07 08:51:42 -0700

    BRB

--- mods/SUPAHMONS/pokedex.js
+++ mods/SUPAHMONS/pokedex.js
@@ -1,1 +1,2 @@
 exports.BattlePokedex = {
+serperior:{num:497,species:"Serperior",types:["Grass","Dragon"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:75,atk:75,def:95,spa:75,spd:95,spe:113},abilities:{0:"Overgrow",DW:"Contrary"},heightm:3.3,weightkg:63,color:"Green",prevo:"servine",evoLevel:36,eggGroups:["Ground","Plant"]},
