a992ffde2b12127da011bf8a3fda0a2475f57b8d
Oiawesome <Oiawesome99@yahoo.com>
2013-07-07 19:19:26 -0700

    Update pokedex.js

--- mods/SUPAHMONS/pokedex.js
+++ mods/SUPAHMONS/pokedex.js
@@ -4,3 +4,4 @@ samurott:{num:503,species:"Samurott",types:["Water","Fighting"],genderRatio:{M:0
 emboar:{num:500,species:"Emboar",types:["Fire","Ground"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:120,atk:130,def:105,spa:100,spd:105,spe:90},abilities:{0:"Sheer Force",DW:"Gluttony"},heightm:1.6,weightkg:150,color:"Red",prevo:"pignite",evoLevel:36,eggGroups:["Ground"]},
 rhydon:{num:112,species:"Rhydon",types:["Dragon","Rock"],baseStats:{hp:125,atk:140,def:145,spa:10,spd:105,spe:65},abilities:{0:"Intimidate",DW:"Rock Head"},heightm:1.9,weightkg:120,color:"Gray",prevo:"rhyhorn",evos:["rhyperior"],evoLevel:42,eggGroups:["Monster","Ground"]},
 pidgeot:{num:18,species:"Pidgeot",types:["Normal","Flying"],baseStats:{hp:100,atk:125,def:95,spa:60,spd:95,spe:155},abilities:{0:"Speed Boost",DW:"Scrappy"},heightm:1.5,weightkg:39.5,color:"Brown",prevo:"pidgeotto",evoLevel:36,eggGroups:["Flying"]},
+
