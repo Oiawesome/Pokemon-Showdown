20740d46282ef6ebeae7874d16a467fc6efa882e
Oiawesome <Oiawesome99@yahoo.com>
2013-07-08 19:33:41 -0700

    Update pokedex.js

--- mods/Usermons/pokedex.js
+++ mods/Usermons/pokedex.js
@@ -1,3 +1,3 @@
 exports.BattlePokedex = {
-oiawesome:{num:151,species:"Oiawesome",types:["Water","Electric"],gender:"N",baseStats:{hp:85,atk:95,def:100,spa:115,spd:90,spe:101},abilities:{0:"Drizzle",1:"Drunken Fist",DW:"OBJECTION"},heightm:0.4,weightkg:4,color:"Brown",eggGroups:["No Eggs"]},
+oiawesome:{num:700,species:"Oiawesome",types:["Water","Electric"],gender:"N",baseStats:{hp:85,atk:95,def:100,spa:115,spd:90,spe:101},abilities:{0:"Drizzle",1:"Drunken Fist",DW:"OBJECTION"},heightm:0.4,weightkg:4,color:"Brown",eggGroups:["No Eggs"]},
 };
