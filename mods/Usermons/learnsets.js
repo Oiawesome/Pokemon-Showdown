36a4f680c08b518507d2f8bffbd475676ebbfd51
Oiawesome <Oiawesome99@yahoo.com>
2013-07-09 14:31:38 -0700

    derp

--- mods/Usermons/learnsets.js
+++ mods/Usermons/learnsets.js
@@ -1,1 +1,3 @@
-*/g
+exports.BattleLearnsets = {
+oiawesome:{learnset:{hydropump:["5L1"],surf:["5L1"],vodkabottlethrow:["5L1"],thunderbolt:["5L1"],thunder:["5L1"],thunderwave:["5L1"],waterspout:["4L1"],scald:["5L1"],discharge:["5L1"]}}
+};
