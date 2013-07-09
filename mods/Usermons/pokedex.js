841dbd9453f334fc36189bbe1c9116eda748d2e4
Oiawesome <Oiawesome99@yahoo.com>
2013-07-08 19:22:13 -0700

    make usermons hoz

---
+++ mods/Usermons/pokedex.js
@@ -0,0 +1,3 @@
+exports.BattlePokedex = {
+mew:{num:151,species:"Mew",types:["Psychic"],gender:"N",baseStats:{hp:100,atk:100,def:100,spa:100,spd:100,spe:100},abilities:{0:"Synchronize"},heightm:0.4,weightkg:4,color:"Pink",eggGroups:["No Eggs"]},
+};
