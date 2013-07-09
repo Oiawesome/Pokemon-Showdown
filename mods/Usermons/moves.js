2c2dbaf4ecd75f657ad4956f44b9ab2ecb5c0a20
Oiawesome <Oiawesome99@yahoo.com>
2013-07-08 20:23:18 -0700

    add vodka bottle throw

--- mods/Usermons/moves.js
+++ mods/Usermons/moves.js
@@ -1,19 +1,19 @@
 exports.BattleMovedex = {
-  "furyattack": {
-		num: 31,
+  "vodkabottlethrow": {
+		num: 800,
 		accuracy: 85,
 		basePower: 15,
-		category: "Physical",
+		category: "Special",
 		desc: "Deals damage to one adjacent target and hits two to five times. Has a 1/3 chance to hit two or three times, and a 1/6 chance to hit four or five times. If one of the hits breaks the target's Substitute, it will take damage for the remaining hits. If the user has the Ability Skill Link, this move will always hit five times. Makes contact.",
 		shortDesc: "Hits 2-5 times in one turn.",
-		id: "furyattack",
-		name: "Fury Attack",
+		id: "vodkabottlethrow",
+		name: "Vodka Bottle Throw",
 		pp: 20,
 		priority: 0,
 		isContact: true,
-		multihit: [2,5],
+		multihit: [3,5],
 		secondary: false,
 		target: "normal",
-		type: "Normal"
+		type: "Water"
 	},
 };
