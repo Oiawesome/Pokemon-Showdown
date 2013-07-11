149c28ca085a80907582a4a9b1b8da420e63e47b
Oiawesome <Oiawesome99@yahoo.com>
2013-07-11 09:40:37 -0700

    Update statuses.js

--- mods/usermonsweather/statuses.js
+++ mods/usermonsweather/statuses.js
@@ -1,1 +1,44 @@
-/*fdfd
+function clampIntRange(num, min, max) {
+  num = Math.floor(num);
+	if (num < min) num = min;
+	if (typeof max !== 'undefined' && num > max) num = max;
+	return num;
+}
+exports.BattleStatuses = {
+  raindance: {
+		effectType: 'Weather',
+		duration: 5,
+		durationCallback: function(source, effect) {
+			if (source && source.item === 'damprock') {
+				return 8;
+			}
+			return 5;
+		},
+		onBasePower: function(basePower, attacker, defender, move) {
+			if (move.type === 'Water') {
+				this.debug('rain water boost');
+				return basePower * 1.5;
+			}
+			if (move.type === 'Fire') {
+				this.debug('rain fire suppress');
+				return basePower * 1.5;
+			}
+		},
+		onStart: function(battle, source, effect) {
+			if (effect && effect.effectType === 'Ability') {
+				this.effectData.duration = 0;
+				this.add('-weather', 'RainDance', '[from] ability: '+effect, '[of] '+source);
+			} else {
+				this.add('-weather', 'RainDance');
+			}
+		},
+		onResidualOrder: 1,
+		onResidual: function() {
+			this.add('-weather', 'RainDance', '[upkeep]');
+			this.eachEvent('Weather');
+		},
+		onEnd: function() {
+			this.add('-weather', 'none');
+		}
+	},
+};
