112f19fa1aa8b0b621bebdfbece1861df1aa154c
Oiawesome <Oiawesome99@yahoo.com>
2013-07-11 09:50:45 -0700

    Update statuses.js

--- mods/usermonsweather/statuses.js
+++ mods/usermonsweather/statuses.js
@@ -5,36 +5,36 @@ function clampIntRange(num, min, max) {
 	return num;
 }
 exports.BattleStatuses = {
-  raindance: {
+  zassoukemuri: {
 		effectType: 'Weather',
 		duration: 5,
 		durationCallback: function(source, effect) {
-			if (source && source.item === 'damprock') {
+			if (source && source.item === 'superjoint') {
 				return 8;
 			}
 			return 5;
 		},
 		onBasePower: function(basePower, attacker, defender, move) {
-			if (move.type === 'Water') {
-				this.debug('rain water boost');
+			if (move.type === 'Grass') {
+				this.debug('weed grass boost');
 				return basePower * 1.5;
 			}
 			if (move.type === 'Fire') {
-				this.debug('rain fire suppress');
+				this.debug('weed fire boost');
 				return basePower * 1.5;
 			}
 		},
 		onStart: function(battle, source, effect) {
 			if (effect && effect.effectType === 'Ability') {
 				this.effectData.duration = 0;
-				this.add('-weather', 'RainDance', '[from] ability: '+effect, '[of] '+source);
+				this.add('-weather', 'ZassouKemuri', '[from] ability: '+effect, '[of] '+source);
 			} else {
-				this.add('-weather', 'RainDance');
+				this.add('-weather', 'ZassouKemuri');
 			}
 		},
 		onResidualOrder: 1,
 		onResidual: function() {
-			this.add('-weather', 'RainDance', '[upkeep]');
+			this.add('-weather', 'ZassouKemuri', '[upkeep]');
 			this.eachEvent('Weather');
 		},
 		onEnd: function() {
