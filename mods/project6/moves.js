3b18bf8a1f5288c520e6a712a1ae1d3bf01e0f87
Oiawesome <Oiawesome99@yahoo.com>
2013-07-12 19:25:22 -0700

    ADD GAME MOD SHIT

--- mods/project6/moves.js
+++ mods/project6/moves.js
@@ -16,15 +16,15 @@ exports.BattleMovedex = {
                 type: "Grass"
         },
         "partingshot": {
-                //num: ???,
-                //accuracy: ???,
-                basePower: 0,
+                num: 1032,
+                accuracy: 100,
+                basePower: 75,
                 category: "Status",
                 desc: "Lowers the opponent's Special Attack and Attack by 1, then switches the user out of battle.",
                 shortDesc: "User switches out after lowering the target's SpA and Atk.",
                 id: "partingshot",
                 name: "Parting Shot",
-                //pp: ???,
+                pp: 25,
                 priority: 0,
                 boosts: {
                         atk: -1,
@@ -33,34 +33,34 @@ exports.BattleMovedex = {
                 selfSwitch: true,
                 secondary: false,
                 target: "normal",
-                //type: ???
+                type: "Fighting"
         },
         "paraboliccharge": {
-                //num: ???,
-                //accuracy: ???,
-                //basePower: ???,
-                //category: ???,
+                num: 1033,
+                accuracy: 100,
+                basePower: 80,
+                category: "Special",
                 desc: "Deals damage to all targets, including its ally. The user recovers half of the HP lost by the target, rounded up. If Big Root is held by the user, the HP recovered is 1.3x normal, rounded half down.",
                 shortDesc: "User recovers 50% of the damage dealt.",
                 id: "paraboliccharge",
                 name: "Parabolic Charge",
-                //pp: ???,
+                pp: 16,
                 priority: 0,
                 drain: [1,2],
                 secondary: false,
-                target: "all",
+                target: "normal",
                 type: "Electric"
         },
         "boomburst": {
-                //num: ???,
-                //accuracy: ???,
-                //basePower: ???,
-                //category: ???,
+                num: 1034,
+                accuracy: 100,
+                basePower: 100,
+                category: "Special",
                 desc: "A sound attack that deals damage to all targets, including its ally.",
                 shortDesc: "Deals damage to all targets.",
                 id: "boomburst",
                 name: "Boomburst",
-                //pp: ???,
+                pp: 16,
                 priority: 0,
                 isSoundBased: true,
                 secondary: false,
@@ -68,18 +68,18 @@ exports.BattleMovedex = {
                 type: "Normal"
         },
         "moonblast": {
-                //num: ???,
-                //accuracy: ???,
-                //basePower: ???,
+                num: 1035,
+                accuracy: 100,
+                basePower: 110,
                 category: "Special",
                 desc: "Deals damage to one adjacent target with a ???% chance to lower Special Attack",
-                shortDesc: "???% chance to lower SpA.",
+                shortDesc: "30% chance to lower SpA.",
                 id: "moonblast",
                 name: "Moonblast",
-                //pp: ???,
+                pp: 25,
                 priority: 0,
                 secondary: {
-                        //chance: ???,
+                        chance: 30,
                         boosts: {
                                 spa: -1
                         }
@@ -88,49 +88,49 @@ exports.BattleMovedex = {
                 type: "Fairy"
         },
         "nobleroar": {
-                //num: ???,
-                //accuracy: ???,
-                basePower: 0,
+                num: 1036,
+                accuracy: 100,
+                basePower: 85,
                 category: "Status",
                 desc: "Lowers the foe's Attack and Special Attack by one stage.",
                 shortDesc: "Lowers SpA and Atk by 1.",
                 id: "nobleroar",
                 name: "Noble Roar",
-                //pp: ???
+                pp: 16
                 priority: 0,
                 boosts: {
                         atk: -1,
                         spa: -1
                 },
                 secondary: false,
-                //target: ???,
-                type: "Normal"
+                target: "normal",
+                type: "Fire"
         },
         "fairywind": {
-                //num: ???,
-                //accuracy: ???,
-                //basePower: ???,
+                num: 1037,
+                accuracy: 100,
+                basePower: 85,
                 category: "Special",
                 desc: "Deals massive damage to all adjacent foes.",
                 shortDesc: "Damages all adjacent foes.",
                 id: "fairywind",
                 name: "Fairy Wind",
-                //pp: ???,
+                pp: 25,
                 priority: 0,
                 secondary: false,
                 target: "allAdjacentFoes",
                 type: "Fairy"
         },
         "topsyturvy": {
-                //num: ???,
-                //accuracy: ???,
+                num: 1038,
+                accuracy: 100,
                 basePower: 0,
                 category: "Status",
                 desc: "Reverses all stat modifications that the foe has.",
                 shortDesc: "Reverses the foe's stat changes.",
                 id: "topsyturvy",
                 name: "Topsy-Turvy",
-                //pp: ???,
+                pp: 16,
                 priority: 0,
                 onHit: function(target) {
                         for (var i in target.boosts) {
@@ -140,18 +140,18 @@ exports.BattleMovedex = {
                 },
                 secondary: false,
                 target: "normal",
-                ///type: ???
+                type: "Fairy"
         },
         "drainingkiss": {
-                //num: ???,
-                //accuracy: ???,
-                //basePower: ???,
-                //category: ???,
+                num: 1039,
+                accuracy: 100,
+                basePower: 80,
+                category: "Special",
                 desc: "Deals damage to one adjacent target. The user recovers half of the HP lost by the target, rounded up. If Big Root is held by the user, the HP recovered is 1.3x normal, rounded half down.",
                 shortDesc: "User recovers 50% of the damage dealt.",
                 id: "drainingkiss",
                 name: "Draining Kiss",
-                //pp: ???,
+                pp: 16,
                 priority: 0,
                 drain: [1,2],
                 secondary: false,
@@ -159,33 +159,45 @@ exports.BattleMovedex = {
                 type: "Fairy"
         },
         "geomancy": {
-                //num: ???,
-                //accuracy: ???,
-                //basePower: ???,
-                //category: ???,
+                num: 1040,
+                accuracy: 100,
+                basePower: 130,
+                category: "Special",
                 desc: "Deal massive damage to one adjacent target.",
                 shortDesc: "No additional effect.",
                 id: "geomancy",
                 name: "Geomancy",
-                //pp: ???,
-                priority: 0,
+                pp: 16,
+        	priority: 0,
+		self: {
+			boosts: {
+				spa: -1
+                                spd: -1
+			}
+		},
                 secondary: false,
-                target: "normal",
+                target: "all",
                 type: "Fairy"
         },
         "oblivionwing": {
-                //num: ???,
-                //accuracy: ???,
-                //basePower: ???,
-                //category: ???,
+                num: 1041,
+                accuracy: 100,
+                basePower: 130,
+                category: "Special",
                 desc: "Deal massive damage to one adjacent target.",
                 shortDesc: "No additional effect.",
                 id: "oblivionwing",
                 name: "Oblivion Wing",
-                //pp: ???,
-                priority: 0,
+                pp: 16,
+        	priority: 0,
+		self: {
+			boosts: {
+				spa: -1
+                                spd: -1
+			}
+		},
                 secondary: false,
-                target: "normal",
+                target: "all",
                 type: "Dark"
         }
 };
